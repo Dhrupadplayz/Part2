8055244a24e16aeb98c57305245f9e0948796b38
Dhrupadplayz <94753811+Dhrupadplayz@users.noreply.github.com>
2021-11-24 16:19:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'png',
+    png_quality: 90,
+
+
+});
+camera = document.getElementById("camera");
+Webcam.attach( '#camera' );
+function take_screenie(){
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML='<img id="caputred_image" src="'+data_url+'">'
+    })
+}
+console.log('ml5.version', ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MZm1mfgA5/",modelLoaded);
